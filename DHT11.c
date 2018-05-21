a0ee9f02ff759126a60e91eb05a5f7dd07002965
Bodo Bauer <bb@bb-zone.com>
2018-05-22 00:20:22 +0200

    update

--- DHT11.c
+++ DHT11.c
@@ -35,7 +35,7 @@ int8_t dht11_get_pulse( uint8_t pin ) {
             retval = ((micros() - t) > 40) ? 1 : 0;
         }
     }
-    printf("pulse: ", retval);
+    printf("pulse: %d\n", retval);
     return retval;
 }
 
