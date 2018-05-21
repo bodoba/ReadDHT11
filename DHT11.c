de232b5b499594745efb24335fa91489b74ac1c9
Bodo Bauer <bb@bb-zone.com>
2018-05-22 00:43:23 +0200

    update

--- DHT11.c
+++ DHT11.c
@@ -41,7 +41,7 @@ int8_t dht11_get_pulse( uint8_t pin ) {
             retval = (lng > 40) ? 1 : 0;
         }
     }
-    printf("P: %lu L %lu H %u %d\n", lowcnt, highcnt, lng, retval);
+    printf("P: %u L %u H %u %d\n", lowcnt, highcnt, lng, retval);
     return retval;
 }
 
